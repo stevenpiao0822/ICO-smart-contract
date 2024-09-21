3bc4048e83803c8f9a274c69a1590cabbf0e1232
stevenpiao0822 <alenschmitt415@gmail.com>
2024-09-21 11:43:18 -0700

    Added openzeppelin library

--- contracts/SampleICO.sol
+++ contracts/SampleICO.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import from node_modules @openzeppelin/contracts v4.0
+// import from node_modules @openzeppelin/contracts v4.0.
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
